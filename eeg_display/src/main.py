01eced4e9bbb3ef17e60106f14a992f64fd0f7bb
Josiah <duhaimejosiah@gmail.com>
2021-08-12 16:35:19 -0700

    remove sleep from eeg_display

--- eeg_display/src/main.py
+++ eeg_display/src/main.py
@@ -62,8 +62,8 @@ if __name__ == '__main__':
     # game loop
     while running:
         # pause to give time to start a recording
-        if counter == 0:
-            time.sleep(15)
+        # if counter == 0:
+        #     time.sleep(15)
 
         # display arrows
         arrows.left_off.load_arrow(screen)
